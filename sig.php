4f017b0de907a231fe7012cf2f0df7073cef94fd
Keith Armstrong <keith1015@gmail.com>
2013-10-03 21:35:40 -0400

    Fixed some line break bugs that snuck in

--- sig.php
+++ sig.php
@@ -1,13 +1,12 @@
 <?php
 
 	//variable inputs
-	define("USERNAME", "TheMaster");
+	define(USERNAME, "TheMaster");
 	
 	//critical values that could change if the website is updated
 	define("REP_POS_OFFSET", 21);
 
-	$url = 
-"http://webdevrefinery.com/forums/members/?sort_key=posts&sort_order=desc&max_results=60&st=";
+	$url = "http://webdevrefinery.com/forums/members/?sort_key=posts&sort_order=desc&max_results=60&st=";
 
 	$page = file_get_contents($url . "0");
 	//$page = $page . file_get_contents($url . "60");
@@ -17,15 +16,13 @@
 USER. IF IT CAN'T FIND YOUR USERNAME.
            UNCOMMENT THE ABOVE TO LINES TO INCREASE THE SEARCH AREA. */
 
-	//finding username and cutting string to start at first 
-occurance of it
+	//finding username and cutting string to start at first occurance of it
 	$CutToUserPos = strpos($page, USERNAME);
 	$CutToUser = substr($page, $CutToUserPos, 1500);
 
 	//finding rep amount
 	$CutToRepPos = strpos($CutToUser, "<span class='number'>");
-	$CutToRep = substr($CutToUser, $CutToRepPos + REP_POS_OFFSET, 
-5);
+	$CutToRep = substr($CutToUser, $CutToRepPos + REP_POS_OFFSET, 5);
 	$RepAmount = filter_var($CutToRep, FILTER_SANITIZE_NUMBER_INT);
 
 	//find days since joined
@@ -51,7 +48,5 @@ day!";
 	$background_color = imagecolorallocate($im, 248, 248, 248);
 	$text_color = imagecolorallocate($im, 0, 0, 255);
 	imagestring($im, 10, 10, 10, $string, $text_color);
-	imagepng($im , "signature.png");
-	imagedestroy($im);	
-
+	imagepng($im);
 ?>
