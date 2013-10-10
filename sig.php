ad0a7e6b637a101c2f7c1cd100e11bcac2a61609
jis2507 <jis2507@gmail.com>
2013-10-10 18:51:05 +1030

    Fixed the color issues with the signature generation.

--- sig.php
+++ sig.php
@@ -10,13 +10,13 @@ $userData = getUserData(USERNAME);
 $RepPerDay = round(($userData->reputation) / floor((time() - $userData->joindate) / 86400), 3);
 
 // Creating the string to be written to the signature image
-$string = USERNAME . " has " . $userData->reputation . " rep points. That's " . $RepPerDay . " rep per day!" . $userStats[Status];
+$string = USERNAME . " has " . $userData->reputation . " rep points. That's " . $RepPerDay . " rep per day!";
 
-$im = imagecreate(800, 110);
+$im = imagecreatetruecolor(800, 110);
 $background_color = imagecolorallocate($im, 248, 248, 248);
+imagefill($im, 0, 0, $background_color);
 $text_color = imagecolorallocate($im, 0, 0, 255);
 imagestring($im, 15, 110, 50, $string, $text_color);
-
 
 // Overlaying the previously generated text only signature, with the user's avatar
 $avatar = imagecreatefromstring(file_get_contents($userData->avatar));
