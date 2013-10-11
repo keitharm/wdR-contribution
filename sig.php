bcaa714a4f6daedc5673618e482e2a295ad11518
jis2507 <jis2507@gmail.com>
2013-10-12 09:40:50 +1030

    Added extra stats to signature.

--- sig.php
+++ sig.php
@@ -6,10 +6,12 @@ header('Content-Type: image/png');
 define("USERNAME", $_GET['user']);
 
 $userData = getUserData(USERNAME);
-
 $state = getUserOnlineState(USERNAME);
 
-$RepPerDay = round(($userData->reputation) / floor((time() - $userData->joindate) / 86400), 3);
+$rep = $userData->reputation;
+$RepPerDay = round(($rep) / floor((time() - $userData->joindate) / 86400), 3);
+$joined = date('l jS \of F\, Y', $userData->joindate);
+$posts = $userData->posts;
 
 // Creating the string to be written to the signature image
 $string = USERNAME . " has " . $userData->reputation . " rep points. That's " . $RepPerDay . " rep per day!";
@@ -18,7 +20,10 @@ $im = imagecreatetruecolor(800, 110);
 $background_color = imagecolorallocate($im, 248, 248, 248);
 imagefill($im, 0, 0, $background_color);
 $text_color = imagecolorallocate($im, 0, 0, 255);
-imagettftext($im, 12.5, 0, 110, 62, $text_color, "fonts/helvetica.ttf", $string);
+imagettftext($im, 13, 0, 110, 40, $text_color, "fonts/helvetica.ttf", $string);
+imagettftext($im, 9, 0, 110, 65, $text_color, "fonts/helvetica.ttf", "Joined: " . $joined);
+imagettftext($im, 9, 0, 110, 80, $text_color, "fonts/helvetica.ttf", "Posts: " . $posts);
+imagettftext($im, 9, 0, 110, 95, $text_color, "fonts/helvetica.ttf", "Reputation: " . $rep);
 
 // Overlaying the previously generated text only signature, with the user's avatar
 $avatar = imagecreatefromstring(file_get_contents($userData->avatar));
