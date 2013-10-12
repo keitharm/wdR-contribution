016776ec941fb7e68af2e271deadafce80167881
jis2507 <jis2507@gmail.com>
2013-10-12 14:46:59 +1030

    Added display of rank to sig.

--- sig.php
+++ sig.php
@@ -12,6 +12,7 @@ $rep = $userData->reputation;
 $RepPerDay = round(($rep) / floor((time() - $userData->joindate) / 86400), 3);
 $joined = date('l jS \of F\, Y', $userData->joindate);
 $posts = $userData->posts;
+$rank = getUserRank(USERNAME);
 
 // Creating the string to be written to the signature image
 $string = USERNAME . " has " . $userData->reputation . " rep points. That's " . $RepPerDay . " rep per day!";
@@ -21,10 +22,10 @@ $bg = imagecreatefrompng("images/sig_background.png");
 imagecopyresampled($im, $bg, 0, 0, 0, 0, 800, 110, 800, 110);
 
 $text_color = imagecolorallocate($im, 0, 0, 255);
-imagettftext($im, 13, 0, 110, 40, $text_color, "fonts/helvetica.ttf", $string);
-imagettftext($im, 9, 0, 110, 65, $text_color, "fonts/helvetica.ttf", "Joined: " . $joined);
-imagettftext($im, 9, 0, 110, 80, $text_color, "fonts/helvetica.ttf", "Posts: " . $posts);
-imagettftext($im, 9, 0, 110, 95, $text_color, "fonts/helvetica.ttf", "Reputation: " . $rep);
+imagettftext($im, 13, 0, 110, 30, $text_color, "fonts/helvetica.ttf", $string);
+imagettftext($im, 13, 0, 110, 52, $text_color, "fonts/helvetica.ttf", "Current Rank: " . $rank);
+imagettftext($im, 9, 0, 110, 75, $text_color, "fonts/helvetica.ttf", "Joined: " . $joined);
+imagettftext($im, 9, 0, 110, 90, $text_color, "fonts/helvetica.ttf", "Posts: " . $posts);
 
 // Overlaying the previously generated text only signature, with the user's avatar
 $avatar = imagecreatefromstring(file_get_contents($userData->avatar));
