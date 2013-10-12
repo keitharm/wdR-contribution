38035be491db844cdeffeefe1ed6450ab7d312a4
Keith Armstrong <keith1015@gmail.com>
2013-10-12 15:46:12 -0400

    so both of our test databases will work until we can get an actual shared server

--- config.php
+++ config.php
@@ -1,6 +1,12 @@
 <?php
 # Database info
-$config['db']['dbname'] = "wdr-contribution";
-$config['db']['username'] = "root";
-$config['db']['password'] = "";
+if (exec("hostname") == "vps.toequest.com") {
+	$config[db][dbname] = "code_wdr";
+	$config[db][username] = "code_wdr";
+	$config[db][password] = "totallynotmyactualpassword:)!";
+} else {
+	$config['db']['dbname'] = "wdr-contribution";
+	$config['db']['username'] = "root";
+	$config['db']['password'] = "";
+}
 ?>
