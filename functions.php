d204f433b1244ac0637fdbb458b04ad82a689962
Keith Armstrong <keith1015@gmail.com>
2014-05-18 17:34:17 -0500

    Improved findall function

--- functions.php
+++ functions.php
@@ -15,15 +15,15 @@ function database() {
 }
 
 function extractData($data, $search, $ending, $specific = -1) {
-	$matches = findall($search, $data);
-	foreach ($matches as &$val) {
-		$offset = 0;
-		$val += strlen($search);
+    $matches = findall($search, $data);
+    foreach ($matches as &$val) {
+        $offset = 0;
+        $val += strlen($search);
         while (substr($data, $val+$offset, strlen($ending)) != $ending) {
             $offset++;
         }
-		$val = substr($data, $val, $offset);
-	}
+        $val = substr($data, $val, $offset);
+    }
     if ($matches == false) {
         return "Error, no matches found.";
     }
@@ -32,38 +32,29 @@ function extractData($data, $search, $ending, $specific = -1) {
         if (count($matches) == 1) {
             return $matches[0];
         }
-	    return $matches;
+        return $matches;
     }
     return $matches[$specific-1];
 }
 
-// Function I found online
-// Rewrote it to look nicer (so many comments in the last version!)
-function findall($needle, $haystack) { 
-    $buffer = '';
-    $pos = 0;
-    $end = strlen($haystack);
-    $getchar = '';
-    $needlelen = strlen($needle); 
-    $found = array();
-    
-    while ($pos < $end) { 
-        $getchar = substr($haystack, $pos, 1);
-        if ($getchar != "\\n" || $buffer < $needlelen) { 
-            $buffer = $buffer . $getchar;
-            if (strlen($buffer) > $needlelen) { 
-                $buffer = substr($buffer, -$needlelen);
-            }
-            if ($buffer == $needle) { 
-                $found[] = $pos - $needlelen + 1;
-            } 
-        } 
-        $pos++;
-    } 
-    if (array_key_exists(0, $found)) { 
-        return $found;
+// Updated function that finds all occurances of needle
+function findall($needle, $haystack) {
+    $pos       = 0;
+    $len       = strlen($haystack);
+    $searchlen = strlen($needle);
+    $results   = array();
+
+    $data = $haystack;
+    while (1) {
+        $occurance = strpos($data, $needle);
+        if ($occurance === false) {
+            return $results;
+        } else {
+            $pos += $occurance+$searchlen;
+            $results[] = $pos-$searchlen;
+            $data = substr($haystack, ($pos));
+        }
     }
-    return false;
 }
 
 // Does the user exists in the total table
